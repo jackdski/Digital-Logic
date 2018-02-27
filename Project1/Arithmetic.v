9ff2340510a5c3231d4fc5fc61882f0e0a7a0489
Jack Danielski <jackdski555@gmail.com>
2018-02-26 18:25:49 -0800

    Update Arithmetic.v

--- Project1/Arithmetic.v
+++ Project1/Arithmetic.v
@@ -2,53 +2,102 @@ module FullAdder(x, y, s, ci, co);
 	input x, y, ci;
 	output s, co;
 	assign s=x^y^ci;
-	assign co=x&y|x&ci|y&ci
+	assign co=x&y|x&ci|y&ci;
 	
 	
 endmodule 
 
-		module Adder(x[3:0], y[3:0], s[3:0], c);
-output s[3:0];
-wire carry[3:0];
-assign carry[0] = 0;
-genvar i;
-generate
-for(i=0; i<4; i++)
-	FullAdder add(x[i],y[i],s[i], c[i], c[i+1])
-endgenerate 
-c = carry[3];
-endmodule 
-
-	module subtractor([3:0] A, [3:0] B, [3:0] out);
-input A, B;
-output out;
-genvar i=0;
-	for(i=0; i<4; i=i+1)
-    if B[i]==0
-        B[i]=1;
-    else
-        B[i]=0;
-	end
-B=B+1;
-FullAdder(A, B, out)
-endmodule
+module Adder(
+	input [3:0] x,
+	input [3:0] y,
+	output [3:0] s,
+	output c
+	);
 	
-module mult_2([7:0] in, [7:0] out, carry):
-	input in;
-	output out, carry;
-	assign carry=0;
-	if in[7]==1
-		assign carry=1;
-	end
-	assign out= in<<1;
-endmodule
-	
-	module div_2([7:0] in, [7:0] out, remain):
-		input in;
-		output out, remain;
-		assign remain=0;
-		if in[0]==1;
-			assign remain=1;
+	wire carry[4:0];
+	assign carry[0] = 0;
+	genvar i;
+	generate
+		for(i=0; i<4; i = i + 1)
+		begin:addbit
+			FullAdder fOne(x[i],y[i],s[i], carry[i], carry[i+1]);
 		end
-		assign out= in>>1;
-	endmodule
+	endgenerate 
+	assign c = carry[3];
+endmodule 
+
+//module subtractor(
+//	input [3:0] A,
+//	input [3:0] B,
+//	output [3:0] out
+//	);
+//	
+//	genvar i;
+//	for(i=0; i<4; i=i+1)
+//	begin:subbit
+//		if (B[i]==0)
+//		begin
+//			B[i]=1;
+//		end
+//		else
+//		begin
+//			B[i]=0;
+//		end
+//	end
+//	assign B = B+1;
+//	FullAdder(A, B, out)
+//endmodule
+	
+module mult_2(in, out, carry);
+	input [7:0] in;
+	output [7:0] out;
+	output carry;
+	
+	wire [8:0] c;
+	reg o;
+	
+	assign c = in << 1;
+	
+	always @(in)
+	begin
+		//assign carry=0;
+		if (in[7] == 1)
+			begin
+				//assign carry = 1;
+				o = 1;
+			end
+		else
+			begin
+				//assign carry = 0;
+				o = 0;
+			end
+	end
+	assign out = c;
+	assign carry = o;
+	
+endmodule
+	
+module div_2(in, out, remain);
+	input [7:0] in;
+	output [7:0] out;
+	output remain;
+	
+	wire [8:0] c;
+	reg o;
+	
+	assign c = in >> 1;
+	
+	always @(in)
+	begin
+		if (in[0] == 1)
+			begin
+				o = 1;
+			end
+		else 
+			begin 
+				o = 0;
+			end
+	end
+	assign out = c;
+	assign remain = o;
+endmodule
