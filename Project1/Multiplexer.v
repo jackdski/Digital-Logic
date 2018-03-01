7ca8727cacae490e799f93b546065e67f4c6d1c3
Jack Danielski <jackdski555@gmail.com>
2018-02-28 19:01:45 -0800

    Update Multiplexer.v

--- Project1/Multiplexer.v
+++ Project1/Multiplexer.v
@@ -1,43 +1,12 @@
 module mux(
-	input A,
-	input B,
-	//input C,
-	//input D, 
-	//input [1:0] Key,
-	output [3:0] disp,
-	output [1:0] f
+	input a,
+	input b, 
+	input c,
+	input d,
+	input [1:0]S,
+	output f
 	);
 	
-	reg g;
-	reg [3:0]h;
-	h = 4'b 0000;
-	if(Key[1] == 0) 
-	begin 
-		if(Key[0] == 0)
-		//begin
-			//g = A;
-			
-		//end
-		else
-		begin 
-			//g = B;
-			h = 4'b0001;
-		end
-	end 
-	else
-	begin 
-		if(Key[0] == 0)
-		begin 
-			//g = C;
-			h = 4'b0010;
-		end
-		else 
-		begin 
-			//g = D;
-			h = 4'b0011;
-		end
-	end
+	assign f = S[1] ? (S[0] ? d:c) : (S[0] ? b : a);
 	
-	assign f = Key;
-	assign disp = h;
-endmodule;
+endmodule
