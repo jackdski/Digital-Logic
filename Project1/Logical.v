f0d27a6ae585ef12af991919f5f5e65242be5f5c
Jack Danielski <jackdski555@gmail.com>
2018-02-28 19:01:20 -0800

    Update Logical.v

--- Project1/Logical.v
+++ Project1/Logical.v
@@ -1,23 +1,68 @@
-module and([3:0]x, [3:0] y, [3:0] out);
-      input x, y;
-      output out;
-      assign out=x & y;
-endmodule
+//module MasterLogical(
+//	input [1:0] SW,
+//	input [3:0] X,
+//	input [3:0] Y,
+//	input [7:0] Z,
+//	output reg [7:0] outOne,
+//	output reg [7:0] outTwo
+//	);
+//	
+//	reg [3:0] o, p;
+//	AND do(X,Y,o);
+//	
+//	always @(*) 
+//	begin 
+//		case(o)
+//		begin 
+//			4'b0000 : outOne[3:0] <= 8'b00000000;
+//			4'b0001 : outOne[3:0] <= 8'b00000001;
+//			4'b0010 : outOne[3:0] <= 8'b00000010;
+//			4'b0011 : outOne[3:0] <= 8'b00000011;
+//		end 
+//		
+//		case(p)
+//		begin 
+//			
+//		end 
+//	end 
+//	
+//	
+//	
+//	
+//endmodule 
 
-module or([3:0]x, [3:0] y, [3:0] out);
-      input x, y;
-      output out;
-      assign out=x | y;
-endmodule
+ module AND(
+	input [3:0] X,
+	input [3:0] Y,
+	output [3:0] out
+	);
+	
+	assign out = X & Y;
+endmodule 
 
-module exor([3:0]x, [3:0] y, [3:0] out);
-      input x, y;
-      output out;
-      assign out=x ^ y;
-endmodule
+ module OR(
+	input [3:0] X,
+	input [3:0] Y,
+	output [3:0] out
+	);
+	
+	assign out = X | Y;
+endmodule 
 
-module not([7:0]z, [7:0] out);
-      input z;
-      output out;
-      assign out= ~z;
-endmodule
+ module XOR(
+	input [3:0] X,
+	input [3:0] Y,
+	output [3:0] out
+	);
+	
+	assign out = X ^ Y;
+endmodule 
+
+ module NOT(
+	input [7:0] Z,
+	output [3:0] outOne,
+	output [3:0] outTwo
+	);
+	
+	assign out = ~Z;
+endmodule 
