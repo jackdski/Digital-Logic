2529b187a0c1ab27aa8668cd77bae79ea64144a7
Jack Danielski <jackdski555@gmail.com>
2018-03-02 02:10:16 -0700

    Update Project1_top.v

--- Project1/Project1_top.v
+++ Project1/Project1_top.v
@@ -1,85 +1,61 @@
 module Project1_top(
 	input [1:0]KEY,
 	input [9:0]SW,
-	//input MAX10_CLK1_50,
+	input MAX10_CLK1_50,
+	//input ADC_CLK_10,
 	output [7:0]HEX0,
 	output [7:0]HEX1,
 	output [7:0]HEX2,
 	output [7:0]HEX3,
+	output [7:0]HEX4,
+	output [7:0]HEX5,
 	output [9:0]LEDR
 	);
-	
-	// final
-//	reg A;
-//	reg B;
-//	always @(posedge KEY[0]) 
-//	begin 
-//		A = ~A;
-//	end 
-//	
-//	always @(posedge KEY[1])
-//	begin 
-//		B = ~B;
-//	end
-//	
-//	wire [1:0]f;
-//	
-//	mux mode(A, B, f)
-	
-	
-	// testing
-	
+		
 	wire [3:0] X, Y; // adder and subtractor inputs
-	wire [7:0] Z; // mult. and divider input
-	wire [7:0] Vone, Vtwo, Vthree, Vfour; // 7 segment inputs
-	wire over; // overflow for subtractor
-	wire carryAdd, carrySub, carryMult, carryDiv; // carries for adder, mult, and divider
+	wire [7:0] Vone, Vtwo, Vthree;
+	wire [9:0] Vfour;
+	wire [1:0] select;
 	
 	
 	assign X[3:0] = SW[3:0];
 	assign Y[3:0] = SW[7:4];
-	assign Z[7:0] = SW[7:0];
+	assign select[1:0] = SW[9:8];
+		
+	reg [1:0] buttons =0;
 	
-	wire c,d;
+	always @(posedge KEY[0])
+		begin
+			buttons = buttons + 1;
+		end
+		
+	wire c;
+	wire [7:0] choice;
+	reg [9:0]LEDT;
 	
-	//Adder myadd(X,Y,Vadd,carryAdd);
-	//Subtractor mySubt(X,Y,Vsub,over);
-	//Mult_2 multiplied(Z, Vmult, carryMult);
-	//Div_2 divided(X,Z,carryDiv);
+	MasterA Arith(X, Y, select, Vone, LED);
+	MasterL Logical(X, Y, select, Vtwo);
+	MasterC Comp(X,Y,select, Vthree);
+	MasterM magical(MAX10_CLK1_50, Vfour);
+		
+	mux choose(Vone, Vtwo, Vthree, 0, buttons, choice);
 	
-	AND log(X, Y, Vone[3:0]);
-	
-	//SevenSegment first(Vone[0], 0, HEX0);
-	//SevenSegment second(Vone[1], 0, HEX1);
-	//SevenSegment third(Vone[2], 0, HEX2);
-	//SevenSegment second(Vone[3], 0, HEX4);
-
-
-	
-	reg o;
-	always @(c)
+	SevenSegment(choice[3:0], HEX0);
+	SevenSegment(choice[7:4], HEX1);
+	SevenSegment(X, HEX2);
+	SevenSegment(Y, HEX3);
+	SevenSegment(select, HEX4);
+	SevenSegment(buttons, HEX5);
+	always @(KEY)
 	begin
-		//assign carry=0;
-		if (c == 1)
-			begin
-				//assign carry = 1;
-				o = 1;
-				//LEDR[9] = 1;
-			end
-		else
-			begin
-				//assign carry = 0;
-				o = 0;
-				//LEDR[9] = 0;
-			end
+	case(buttons)
+		0: LEDT[9] <= LED;
+		1: LEDT[9:0] <=0;
+		2: LEDT[9:0] <=0;
+		3: LEDT[9:0] <= Vfour;
+	endcase
 	end
 	
-	assign LEDR[9] = o;
-	
-//	always @(posedge KEY[0])
-//	begin 
-//		LEDR[0] = 1;
-//	end
-
+	assign LEDR= LEDT;
 
 endmodule 
