4d70c8da51a1904ba109269ca0a684389540f208
Jack Danielski <jackdski555@gmail.com>
2018-04-12 17:22:19 -0600

    Update BCD_counter.v

--- Project2/BCD_counter.v
+++ Project2/BCD_counter.v
@@ -1,57 +1,39 @@
-module bcdCounter( 
-	input binaryTime[10:0],
-	input clk,
-	output reg [3:0] seconds,
-	output reg [3:0] tenths,
-	output reg [3:0] hundreths,
-	output reg [3:0] thousandths
-	);
-
-	parameter reset = 4'b1001;
-	reg [10:0] num;
-	
-	clockdivider(clk, binaryTime[10:0], 1, 0, num[10:0]);
-	
-	if (num[3] & num[0]) // if num = 9
-	begin
-		tenths = 
-	end
-	
-	
-	always @ (
-		
-
-endmodule
-
-//2bit counter
+// helped by book 5.14
 module BCDCount(
-	input clk,
-	input clear,
-	input E,
+	input clkDivder,
+	output reg [3:0]BCD3,
+	output reg [3:0]BCD2,
 	output reg [3:0]BCD1,
 	output reg [3:0]BCD0
 	);
 	
 	always @(posedge clk)
 	begin
-		if(clear)
+		if(BCD0 == 4'b1001)
 		begin
-			BCD1 <= 0;
 			BCD0 <= 0;
-		end
-		else if(E)
-			if(BCD0 == 4'b1001)
+			if(BCD1 == 4'b1001)
 			begin
-				BCD <= 0;
-				if(BCD1 == 4'b1001)
-					BCD1 <= 0;
+				BCD1 <= 0;
+				if(BCD2 == 4'b1001)
+				begin
+					BCD2 <= 0;
+					if(BCD3 == 4'b1001)
+						BCD3 <= 0;
+					else
+						BCD3 <= BCD3 + 1;
+				end
 				else
-					BCD1 <= BCD1 + 1;
+				begin 
+					BCD2 <= BCD2 + 1;	
+				end
 			end
-			else 
-			BCD0 <= BCD0 + 1;
+			else
+			begin
+				BCD1 <= BCD1 + 1;
+			end
 		end
-	endmodule
-	
+		else 
+		BCD0 <= BCD0 + 1;
 	
 endmodule 
