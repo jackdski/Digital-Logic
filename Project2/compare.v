c60389f7666c40ae3fc5e2e23f3777324d6765c1
Jack Danielski <jackdski555@gmail.com>
2018-04-12 20:42:59 -0700

    Update compare.v

--- Project2/compare.v
+++ Project2/compare.v
@@ -1,18 +1,25 @@
 module Compare(
-input [10:0] in,
-input clk,
-output [10:0] val
-);
+	input [10:0] in,
+	input clk,
+	output [10:0] val
+	);
 
-reg cutoff= 11'b01000000000;
+	reg cutoff= 11'b01000000000;
+	reg [10:0] temp;
+	reg [10:0] temp2;
+	
+	lfsr(clk, temp2);
+	always@(posedge clk)
+	begin
+		if (in < cutoff)
+			begin
+				temp = temp2;
+			end
+		else
+			begin
+				temp = in;
+			end
+	end
+	assign val = temp;
 
-always@(posedge clk)
-if (in < cutoff)
-	begin
-		lfsr(clk);
-	end
-else
-	begin
-		val <= in;
-	end
 endmodule 
